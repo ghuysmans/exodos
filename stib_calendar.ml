7090b480bc65f2c9a7eea99ff142fc287333506f
Guillaume Huysmans <ghuysmans99@gmail.com>
2018-07-01 23:25:12 +0200

    Simplified wrapping 2

--- stib_calendar.ml
+++ stib_calendar.ml
@@ -7,17 +7,13 @@ let fmt = Tyre.(
   <* stop
 )
 
-let of_json s =
+let wrap s =
   match Tyre.(exec (compile fmt) s) with
   | Ok (((((year, month), day), hour), minute), second) ->
     CalendarLib.Calendar.lmake ~year ~month ~day ~hour ~minute ~second ()
   | Error _ -> failwith "Stib_calendar.of_json"
 
-let to_json t =
+let unwrap t =
   Tyre.eval fmt CalendarLib.Calendar.(((((
     year t, CalendarLib.Date.int_of_month (month t)), day_of_month t),
     hour t), minute t), second t)
-
-
-let of_json_o = Option.map of_json
-let to_json_o = Option.map to_json
