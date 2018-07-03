321b409b55a6c437eef1387c42604616f3726ea5
Guillaume Huysmans <ghuysmans99@gmail.com>
2018-07-03 23:03:51 +0200

    Time zone (to be improved?)

--- routeserver_calendar.ml
+++ routeserver_calendar.ml
@@ -1,5 +1,3 @@
-(* FIXME handle time zones? *)
-
 type t = CalendarLib.Calendar.t
 
 let fmt = Tyre.(
@@ -12,12 +10,14 @@ let fmt = Tyre.(
 
 let wrap s =
   match Tyre.(exec (compile fmt) s) with
-  | Ok ((((((((year, month), day), hour), minute), second), d), th), tm) ->
-    CalendarLib.Calendar.lmake ~year ~month ~day ~hour ~minute ~second ()
-  | Error _ -> failwith "Calendar.of_string"
+  | Ok ((((((((year, month), day), hour), minute), second), d), th), 0) ->
+    let open CalendarLib in
+    Calendar.lmake ~year ~month ~day ~hour ~minute ~second () |>
+    Time_Zone.(on Calendar.to_gmt (UTC_Plus th))
+  | _ -> failwith "Calendar.of_string"
 
 let unwrap t =
-  Tyre.eval fmt CalendarLib.Calendar.((((((((
-    year t, CalendarLib.Date.int_of_month (month t)), day_of_month t),
+  Tyre.eval fmt CalendarLib.(Calendar.(((((((
+    year t, Date.int_of_month (month t)), day_of_month t),
     hour t), minute t), second t),
     `Right ()), 0), 0)
--- time.ml
+++ time.ml
@@ -3,6 +3,7 @@ open CalendarLib
 type t = Calendar.t
 
 let to_string t =
+  let t = Time_Zone.(on Calendar.from_gmt Local t) in
   Calendar.(Printf.sprintf "%02d:%02d" (hour t) (minute t))
 
 let conv =
@@ -10,7 +11,8 @@ let conv =
     let re = Tyre.(compile (start *> int <&> char ':' *> int <* stop)) in
     match Tyre.exec re s with
     | Ok (h, m) ->
-      Ok (Calendar.create (Date.today ()) (Time.make h m 0))
+      Ok (Calendar.create (Date.today ()) (Time.make h m 0) |>
+          Time_Zone.(on Calendar.to_gmt Local))
     | Error _ ->
       Error (`Msg "invalid time (expected hh:mm)")
   in
